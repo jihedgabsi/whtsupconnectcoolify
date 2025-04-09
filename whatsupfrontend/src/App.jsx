08dd804df9219865549af87c21abb08bbfa5fe76
jihedgabsi <152264496+jihedgabsi@users.noreply.github.com>
2025-04-09 11:44:59 +0100

    Update App.jsx

--- whatsupfrontend/src/App.jsx
+++ whatsupfrontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Whatsup from "./pages/whatsup.jsx";
+import Whatsup from "./pages/whatsup";
 
 
 const App = () => {
